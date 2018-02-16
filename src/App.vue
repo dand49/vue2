83c58f778989ccc61fbe2930e053f0e0260a3a65
Dan Dorton <ddorton@xmission.com>
2018-02-16 09:42:37 -0700

    Testing Vue components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <test></test>
   </div>
 </template>
 
 <script>
+import Test from "./components/test";
 export default {
-  name: 'App'
-}
+  name: "App",
+  components: {
+    Test
+  }
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/test.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="test">
+    <span v-html="title"></span>
+    {{title}}
+    <p>{{user.firstName}}</p>
+    <p v-text="user.lastName"></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "test",
+  data() {
+    return {
+      title: "<h1>Hello World!</h1>",
+      user: {
+        firstName: "John",
+        lastName: "Doe"
+      }
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
