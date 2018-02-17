ad76c0936b66fdf5188274861da5b032f79eb884
Dan Dorton <ddorton@xmission.com>
2018-02-16 18:03:51 -0700

    Playing with routes

--- src/components/test.vue
+++ src/components/test.vue
@@ -1,25 +1,70 @@
 <template>
   <div class="test">
-    <span v-html="title"></span>
-    {{title}}
-    <p>{{user.firstName}}</p>
+    <input type="text" v-model="title"><br />
+    <h1>{{title}}</h1>
+    <p v-if="showName">{{user.firstName}}</p>
     <p v-text="user.lastName"></p>
+    <ul>
+      <li v-for="item in items">{{item.title}}</li>
+    </ul>
+    <button v-on:click="greet('Not your day?')">Say Greeting</button>
+    <br />
+    <input type="text" v-on:keyup="pressKey" v-on:keyup.enter="enterHit">
+    <hr />
+    <label>First Name:</label>
+    <input type="text" v-model="user.firstName">
+    <br />
+    
+   <label>Last Name:</label>
+    <input type="text" v-model="user.lastName">
+    <br />
+    <h3>{{fullName}}</h3>
+    <h2>{{msg}}</h2>
   </div>
 </template>
 
 <script>
 export default {
   name: "test",
+  props: {
+    msg: {
+      type: String,
+      default: 'Nothing to See'
+    }
+  },
   data() {
     return {
-      title: "<h1>Hello World!</h1>",
+      title: "Hello World!",
       user: {
         firstName: "John",
         lastName: "Doe"
-      }
-    };
+      },
+      showName: true,
+      items: [
+        {title: 'Item One'},
+        {title: 'Item Two'},
+        {title: 'Item Three'}
+      ]
+    }
+  },
+  methods: {
+    greet: function(greeting){
+      alert(greeting);
+    },
+    pressKey: function(e) {
+     console.log(e.target.value);
+     //console.log('pressed');
+    },
+    enterHit: function() {
+      console.log('Enter Key Pressed');
+    }
+  },
+  computed: {
+    fullName: function() {
+      return this.user.firstName + ' ' + this.user.lastName;
+    }
   }
-};
+}
 </script>
 
 <style scoped>
